0e8455973698116e8f1a3efddc07a3ce7eedc73f
DLauWJ <102398736+ChuckleBuckle11@users.noreply.github.com>
2023-02-04 11:54:35 +0800

    Added search function with weather data json

--- dist/main.js
+++ dist/main.js
@@ -7,8 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("//weather app API key 32cc350ce67f8346a60c40673c36dfff\n\n\n\nconst searchField = document.querySelector('#city-search');\nconst searchButton = document.querySelector(`#search-button`);\n\n\nsearchButton.onclick = fetchWeather;\n\n\nasync function fetchWeather(){\n    let searchCity = searchField.value;\n    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=32cc350ce67f8346a60c40673c36dfff`, {mode: 'cors'});\n    const weatherData = await response.json();\n    console.log(weatherData);\n    return weatherData;\n}\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
 /******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
 /******/ })()
 ;
