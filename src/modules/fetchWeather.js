5aa39a2be6ae5a5949cf5ee481797167701499ba
DLauWJ <102398736+ChuckleBuckle11@users.noreply.github.com>
2023-02-13 15:36:40 +0800

    Added loading screen

--- src/modules/fetchWeather.js
+++ src/modules/fetchWeather.js
@@ -2,7 +2,8 @@ import updateDisplay from "./displayController";
 
 const searchField = document.querySelector('#search-box');
 const content = document.querySelector(`#content`);
-
+const loadingScreen = document.querySelector('#loading-screen');
+const mainWeatherContent = document.querySelector("#main-weather-content");
 async function fetchWeather() {
     try {
         //if search bar is empty, return and trigger invalid
@@ -14,7 +15,8 @@ async function fetchWeather() {
         // Gets City value in search bar
         let searchCity = searchField.value;
         let unit = "metric"; // display units in json, metric/imperial/kelvin
-        
+        mainWeatherContent.textContent = ""
+        displayLoading();
         let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=32cc350ce67f8346a60c40673c36dfff&units=${unit}`, { mode: 'cors' });
         
         if (!response.ok){ //throws an error if city does not exist
@@ -23,12 +25,27 @@ async function fetchWeather() {
         }
 
         const weatherData = await response.json();
+        // hideLoading();
         updateDisplay(weatherData);
         return weatherData;
     }
     catch(error){
         console.log(error)
     }
+}
+
+function displayLoading(){
+    
+    mainWeatherContent.appendChild(loadingScreen);
+    loadingScreen.classList.add("display");
+    // to Stop loading after some time
+    setTimeout(()=>{
+        hideLoading();
+    },5000);
+}
+
+function hideLoading(){
+    loadingScreen.classList.remove("display");
 }
 
 export default fetchWeather;
